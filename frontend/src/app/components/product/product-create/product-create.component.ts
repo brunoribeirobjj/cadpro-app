180fc11167206844afe2f785b38497ba608e2591
Bruno Ribeiro <brunoribeirobjj@gmail.com>
2024-01-20 01:12:16 -0300

    resolução de chamada price,para ser aplicado a chamada foi necessário colocar uma negação após null

--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -12,7 +12,7 @@ export class ProductCreateComponent implements OnInit {
 
   product: Product = {
     name: '',
-    price: 123.00
+    price: null!
   }
 
   constructor(private productService: ProductService,
