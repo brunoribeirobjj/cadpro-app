650182a8fbcc2d992ea4814606b7b6d5eca6fcef
Bruno Ribeiro <brunoribeirobjj@gmail.com>
2024-01-22 02:33:20 -0300

    Aplicação completa

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -31,9 +31,12 @@ import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
 
+import localePt from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
+import { ProductDeleteComponent } from './components/product/product-delete/ProductDeleteComponent';
 
-
-
+registerLocaleData(localePt);
 
 @NgModule({
   declarations: [
@@ -47,7 +50,9 @@ import { MatSortModule } from '@angular/material/sort';
     ForDirective,
     ProductCreateComponent,
     ProductReadComponent,
-    ProductRead2Component
+    ProductRead2Component,
+    ProductUpdateComponent,
+    ProductDeleteComponent
   ],
   imports: [
     BrowserModule,
@@ -68,7 +73,10 @@ import { MatSortModule } from '@angular/material/sort';
     MatSortModule,
     
   ],
-  providers: [],
+  providers: [{
+    provide: LOCALE_ID,
+    useValue: 'pt-BR'
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- frontend/src/app/views/product-crud/product-crud.component.ts
+++ frontend/src/app/views/product-crud/product-crud.component.ts
@@ -1,22 +1,27 @@
+import { HeaderService } from './../../components/template/header/header.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-product-crud',
   templateUrl: './product-crud.component.html',
-  styleUrl: './product-crud.component.css'
+  styleUrls: ['./product-crud.component.css']
 })
 export class ProductCrudComponent implements OnInit {
 
-  constructor(private router: Router) { }
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
+  constructor(private router: Router, private headerService: HeaderService) {
+    headerService.headerData = {
+      title: 'Cadastro de Produtos',
+      icon: 'storefront',
+      routeUrl: '/products'
+    }
   }
-  
+
+  ngOnInit(): void {
+  }
+
   navigateToProductCreate(): void {
     this.router.navigate(['/products/create'])
   }
-
-
 
 }
